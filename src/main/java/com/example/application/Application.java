b6eae0994ea63ae51c1b374bc989ec966333b4aa
Vesa Nieminen <vesa@vaadin.com>
2022-11-15 14:36:23 +0200

    Initial commit for the issue

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,5 +1,6 @@
 package com.example.application;
 
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
@@ -19,6 +20,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @PWA(name = "Chart style fix", shortName = "Chart style fix", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@JsModule("src/prefers-color-scheme.js")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
